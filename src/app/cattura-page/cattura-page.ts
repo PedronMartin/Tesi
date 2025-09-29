e64eda39f20144a1266ef5e69ceb13a77746ae55
Martin <martingiuseppe.pedron@studenti.univr.it>
2025-09-29 19:05:38 +0200

    Seconda finestra simile alla prima creata. Segue passaggio ad una card sola nella Home

--- src/app/cattura-page/cattura-page.ts
+++ src/app/cattura-page/cattura-page.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { SharedService } from '../shared';
 import { JsonPipe } from '@angular/common';
+import { Toolbar } from "../components/toolbar/toolbar";
+import { MapLayerComponent } from "../components/map-layer/map-layer";
+import { InstCardList } from '../components/inst-card-list/inst-card-list';
 
 @Component({
   selector: 'app-cattura-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, Toolbar, MapLayerComponent, InstCardList],
   templateUrl: './cattura-page.html',
   styleUrl: './cattura-page.css'
 })
