55176824507af884815c0691f3501c90ff239ce0
Martin <martingiuseppe.pedron@studenti.univr.it>
2025-10-07 15:37:31 +0200

    Creazione Server python per backend

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomePage } from './home-page/home-page';
-import { CatturaPage } from './cattura-page/cattura-page';
-
-export const routes: Routes = [
-  { path: '', component: HomePage },
-  { path: 'cattura', component: CatturaPage },
-];
--- src/typings.d.ts
+++
@@ -1,4 +0,0 @@
-// Dichiarazione custom per il modulo 'leaflet-search'.
-// Questo file è necessario perché il pacchetto 'leaflet-search' non fornisce tipi TypeScript ufficiali.
-// Serve solo a evitare errori di tipizzazione durante l'import e l'uso del plugin.
-declare module 'leaflet-search';
