482ffe30e90c072f297801a2d32d5180f8fc7bcb
Martin <martingiuseppe.pedron@studenti.univr.it>
2025-11-13 10:03:45 +0100

    Rimozione file di cache caricati per errore

--- application/greenratingalgorithm/vite/deps/leaflet.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_leaflet_src
-} from "./chunk-JT6JM56J.js";
-import "./chunk-H2SRQSE4.js";
-export default require_leaflet_src();
--- application/greenratingalgorithm/vite/deps_ssr/leaflet.js
+++
@@ -1,6 +0,0 @@
-import { createRequire } from 'module';const require = createRequire(import.meta.url);
-import {
-  require_leaflet_src
-} from "./chunk-2AQRN4JI.js";
-import "./chunk-6DU2HRTW.js";
-export default require_leaflet_src();
