7c2a9f68ffc8b43a14c8d132cbab8b0a3039b3c0
Martin <martingiuseppe.pedron@studenti.univr.it>
2026-02-18 16:02:12 +0100

    Aggiuta licenza generale progetto Open-Source, file cff per citazione e AUTHORS

---
+++ AUTHORS.md
@@ -0,0 +1,10 @@
+# Authors
+
+## Developer & Architect
+* **Martin Giuseppe Pedron** - *Backend & Frontend Implementation* - pedronmartin64@gmail.com
+
+## Contributors
+* Davide Quaglia - *Supervision*
+* Alessio Minici - *Scientific Support*
+* Enrico Antonini - *Data Provider for YOLO trees*
+* OpenStreetMaps - *Main Data Provider*
--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 [Martin Giuseppe Pedron]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -14,4 +30,10 @@ export class App {
 
   //costruisco lo shared in modo che l'html app veda la variabile isLoading durante le richieste api
   constructor(public shared: SharedService) {}
+
+  ngOnInit() {
+    console.log("Developed by Martin Giuseppe Pedron for Thesis in Computer Science, 2026 at University of Verona.");
+    console.log("GitHub: https://github.com/PedronMartin/greenRatingAlgorithm");
+    console.log("Please, if you find any bug or have any suggestion, don't hesitate to contact me! Personal Mail: pedronmartin64@gmail.com");
+  }
 }
